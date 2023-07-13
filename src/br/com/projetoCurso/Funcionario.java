8410930e786ac0b647b978e608f4f3ffd636c9ae
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-13 17:58:54 -0300

    Criação de classe Funcionario para exemplificar usos de Getters and Setters.

---
+++ src/br/com/projetoCurso/Funcionario.java
@@ -0,0 +1,23 @@
+package br.com.projetoCurso;
+
+public class Funcionario {
+	private String nome;
+	private boolean ativo;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String n) {
+		this.nome = n;
+	}
+	
+	public boolean isAtivo() {
+		return ativo;
+	}
+	
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
