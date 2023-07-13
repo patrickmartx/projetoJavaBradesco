c4480d6e00e5420ef0a300cdc9f50f2c61dec752
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-13 11:42:17 -0300

    Atualização do projeto adicionando as classes a um pacote.

---
+++ src/br/com/projetoCurso/cachorro.java
@@ -0,0 +1,9 @@
+package br.com.projetoCurso;
+public class cachorro {
+	int tamanho;
+	String raca;
+	
+	void latir() {
+		System.out.println("Au, Au, Au, Au!!!!!");
+	}
+}
---
+++ src/br/com/projetoCurso/cachorroteste.java
@@ -0,0 +1,13 @@
+package br.com.projetoCurso;
+public class cachorroteste {
+
+	public static void main(String[] args) {
+
+		cachorro beagle = new cachorro();
+		beagle.raca = "Beagle";
+		beagle.tamanho = 40;
+		beagle.latir();
+
+	}
+
+}
