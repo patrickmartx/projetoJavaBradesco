61017d9a4d780cb30d1e363cdf0f569bdc93ecc8
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-13 17:52:55 -0300

    Classe matemática com métodos que utilizam retorno e MatematicaTeste para a execução dos métodos.

---
+++ src/br/com/projetoCurso/MatematicaTeste.java
@@ -0,0 +1,16 @@
+package br.com.projetoCurso;
+
+public class MatematicaTeste {
+
+	public static void main(String[] args) {
+
+		Matematica m = new Matematica();
+		int maior = m.maior(20, 30);
+		System.out.println(maior);
+		
+		double som = m.soma(50, 60);
+		System.out.println(som);
+
+	}
+
+}
