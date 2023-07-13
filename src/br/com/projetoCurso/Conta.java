cab61403a316f71f97feb798fefbe16d4c71caf6
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-13 11:59:00 -0300

    Classe responsável por definir a conta de um cliente no banco e as ações que o mesmo pode executar.

---
+++ src/br/com/projetoCurso/Conta.java
@@ -0,0 +1,25 @@
+package br.com.projetoCurso;
+
+public class Conta {
+	
+	String cliente;
+	double saldo;
+	
+	void exibesaldo() {
+		System.out.println(cliente + " seu saldo é " + saldo);
+	}
+	
+	void saque(double valor) {
+		saldo -= valor;
+	}
+	
+	void deposita(double valor) {
+		saldo += valor;
+	}
+	
+	void transferenciaValor(Conta destino, double valor) {
+		this.saque(valor);
+		destino.deposita(valor);
+	}
+
+}
